cd0d928f2978b7d4088aabb057bb55b332da92a3
Daniel Okonma <51037438+okonma01@users.noreply.github.com>
2023-05-11 16:31:27 -0400

    Edit README, index.html, src/index.js, styles.css
    I updated the README.md file, index.html, src/index.js, and styles.css files.
    
    README:
    - Added a description of the project
    - Added a link to theodinproject.org
    
    index.html:
    - Added a Change grid button
    
    src/index.js:
    - Added an event listener to the Change grid button
    - Added a function to prompt the user for a new grid size
    - Added a function to replace the current grid with a new grid
    - Now when a cell is clicked, the cell's bg color goes through the rainbow colors and back
    
    styles.css:
    - Added styling to the body element
    - Moved the grid styling to src/index.js, to dynamically set the grid size
    - Added animation to grid cells
    - Added an active class
    - Added a rainbow class, which changes the background color of the grid cells

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # etch-a-sketch
+# A simple etch-a-sketch page
+
+# This is a simple etch-a-sketch that is made with HTML, CSS, and JavaScript. It is a project from The Odin Project's curriculum. The project can be found here: https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project?ref=lnav
+
+# The project is a simple etch-a-sketch that allows the user click on a square and change the color of the square. The user can also change the number of squares on the grid.
+
+# The project was made with HTML, CSS, and JavaScript. The HTML and CSS were used to create the grid and the JavaScript was used to add the functionality to the grid.
+
+# The project was made by me, but I did use some code from The Odin Project's curriculum to help me with the project. The code can be found here: https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project?ref=lnav
--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-const body = document.querySelector("body");
-body.style.margin = "0";
-// body.style.border = "1px solid black";
+const n = 16;
+const MAX_SQUARES = n;
 
-const containerDiv = document.querySelector(".container");
+let containerDiv = document.querySelector(".container");
+containerDiv.setAttribute("style",
+    `grid-template-columns: repeat(${n}, 1fr);
+grid-template-rows: repeat(${n}, 1fr)`);
 
-for (let i = 0; i < (16*16); i++) {
-    const childDiv = document.createElement("div");
-    childDiv.classList.add("pad");
-    childDiv.style.border = "1px solid darkcyan";
-    containerDiv.appendChild(childDiv);
+function makeGrid(n) {
+    containerDiv.setAttribute("style", `grid-template-columns: repeat(${n}, 1fr);
+                                        grid-template-rows: repeat(${n}, 1fr)`);
+    for (let i = 0; i < Math.pow(n, 2); i++) {
+        const childDiv = document.createElement("div");
+        childDiv.classList.add("pad");
+        childDiv.style.border = "1px solid darkcyan";
+        childDiv.addEventListener("mouseenter", e => {
+            childDiv.style.backgroundColor = "#fff";
+            childDiv.classList.add("active");
+        });
+        childDiv.addEventListener("mouseleave", e => {
+            childDiv.style.removeProperty("background-color");
+            childDiv.classList.remove("active");
+        });
+        childDiv.addEventListener("click", e => {
+            childDiv.classList.toggle("rainbow");
+        });
+        containerDiv.appendChild(childDiv);
+    }
+}
+
+makeGrid(n);
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", e => {
+    let msg = "How many squares per side do you want for the new grid?";
+    let response = this.prompt(msg);
+    if (response === "") {
+        return;
+    }
+    while (Number.isNaN(response) || !Number.isInteger(response) || parseInt(response) < 1 || parseInt(response) > MAX_SQUARES) {
+        response = this.prompt(`Invalid input. Enter an integer between 1 and ${MAX_SQUARES}:`);
+        if (response === "") {
+            return;
+        }
+        response = Number(response);
+    }
+
+    removeAllChildNodes(containerDiv)
+
+    makeGrid(response);
+});
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
